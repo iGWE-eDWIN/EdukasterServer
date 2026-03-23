19922d3183ba23775b1698257d37e4473e89b0fd
Edwin Azubuike Igwe <igweedwin15@gmail.com>
2026-03-23 05:59:05 -1000

    defaultQuote --fix

--- src/controller/quote.js
+++ src/controller/quote.js
@@ -4,31 +4,16 @@ const createOrUpdateQuote = async (req, res) => {
 try {
     const {text} = req.body
      // Default educational poem (fallback)
-    const defaultQuote = `
-Education is the light that breaks the darkest night,
-A steady flame that guides the will to what is right.
-It builds the mind, refines the soul, expands your view,
-And shows the world what discipline and dreams can do.
+   
 
-Through every page you read and every lesson learned,
-A seed of greatness quietly is being earned.
-Not all rewards are seen at once, nor quickly gained,
-But every effort writes a future well sustained.
-
-So stay consistent, even when the road feels long,
-For knowledge turns the weak into the truly strong.
-With every step, let curiosity lead your way,
-And shape a brighter, wiser version of today.
-    `.trim();
-
-//  if (!text) {
-//       return res.status(400).json({ message: 'Quote text is required' });
-//     }
-
-     // Use admin text OR fallback
-    if (!text || text.trim() === '') {
-      text = defaultQuote;
+ if (!text) {
+      return res.status(400).json({ message: 'Quote text is required' });
     }
+
+    //  // Use admin text OR fallback
+    // if (!text || text.trim() === '') {
+    //   text = defaultQuote;
+    // }
 
      // Deactivate old quote
     await DailyQuote.updateMany({}, { isActive: false });
@@ -52,10 +37,29 @@ And shape a brighter, wiser version of today.
 const getDailyQuote = async (req, res) => {
     try {
          const quote = await DailyQuote.findOne({ isActive: true });
+ const defaultQuote = `
+Education is the light that breaks the darkest night,
+A steady flame that guides the will to what is right.
+It builds the mind, refines the soul, expands your view,
+And shows the world what discipline and dreams can do.
 
+Through every page you read and every lesson learned,
+A seed of greatness quietly is being earned.
+Not all rewards are seen at once, nor quickly gained,
+But every effort writes a future well sustained.
+
+So stay consistent, even when the road feels long,
+For knowledge turns the weak into the truly strong.
+With every step, let curiosity lead your way,
+And shape a brighter, wiser version of today.
+    `
           if (!quote) {
-      return res.status(404).json({ message: 'No quote found' });
+      // return res.status(404).json({ message: 'No quote found' });
+    return res.json({
+      text: defaultQuote
+    })
     }
+
 
      res.json(quote);
     } catch (error) {
